3dd3c4163149f23bf4275ca00d31dafad0852b1d
Sandakalum_Perera <125434080+thikalpha123@users.noreply.github.com>
2025-06-30 16:45:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const defaultConfig = {
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
   ALIVE_MSG: "*Hello👋 ALPHA SL Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
-  GEMINI_API_KEY: "", // Add Gemini API key here
+  GEMINI_API_KEY: "AIzaSyDzCD_T2SppeVNZsOEKe_HwZOyCCKQY04g", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
   WEATHER_API_KEY: "", // Add your weather API key here
   BOT_OWNER: "94762491926", // Replace your bot owner number here with 94(country code)
